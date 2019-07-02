1afa3f9df6cd0be7875a6596ad8e2960c685602c
ihatch <ihatcher@esidesign.com>
2019-07-02 15:09:15 -0400

    cleanup

--- themes/esi-theme/page-latest.php
+++ themes/esi-theme/page-latest.php
@@ -6,14 +6,32 @@
  */
 
 $context = Timber::get_context();
+
 $args = array(
-    'post_type' => 'post',
+    'post_type'      => 'post',
     'posts_per_page' => 12,
-    'paged' => $params['paged']
+    'paged'          => $params['paged']
 );
+
 $context['categories'] = Timber::get_terms('category');
-$context['posts'] = new Timber\PostQuery($args);
-$context['page'] = $params['paged'];
+$context['posts']      = new Timber\PostQuery($args);
+$context['page']       = $params['paged'];
+
+
+//$instagram = array(
+//    'post_type'      => 'instagram',
+//    'order'          => 'DESC',
+//    'posts_per_page' => 3,
+//    'tax_query'      => array(
+//        array(
+//            'taxonomy' => 'instagram_tag',
+//            'field'    => 'slug',
+//            'terms'    => 'esipeople',
+//        ),
+//    ),
+//);
+//
+//$context['instagram_posts'] = Timber::get_posts($instagram);
 
 
 // echo "<pre>";var_dump($context["posts"]);echo "</pre>";die();
